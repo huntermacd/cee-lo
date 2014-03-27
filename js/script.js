e28b6d6bd90d70f4e1a1371ec491f8675e56701a
Hunter MacDermut <whosebluesanyway@gmail.com>
2014-03-26 22:15:41 -0400

    Built out remaining functions. Next up is creation of the game loop.

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ var banker = {
 };
 
 var pot = 0;
+var gameEnd = false;
 
 // Global DOM Elements
 
@@ -53,16 +54,24 @@ function bet(){
 		banker.chips = 0;
 	}
 
+	el_bet.disabled = true;
+	el_bet.style.backgroundColor = "rgba(255, 100, 100, 0.5)";
+	el_roll.disabled = false;
+	el_roll.style.backgroundColor = "rgba(100, 255, 100, 0.5)";
+	update();
+	el_status.innerHTML = "The player has bet " + player.bet + " chips.\nThe banker has bet " + banker.bet + " chips.";
+}
+
+function update(){
 	el_pot.innerHTML = pot;
 	el_playerChips.innerHTML = player.chips;
 	el_bankerChips.innerHTML = banker.chips;
-
-	el_status.innerHTML = "The player has bet " + player.bet + " chips.\nThe banker has bet " + banker.bet + " chips.";
 }
 
 function roll(){
 
 	var counter = 0;
+	var result = [];
 
 	var timer = setInterval(function(){
 
@@ -137,29 +146,66 @@ function roll(){
 
 		if(counter === 12){
 			clearInterval(timer);
-			player.roll = [randNum1, randNum2, randNum3];
-			banker.roll = [Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1];
-			el_status.innerHTML = "The player rolled: " + player.roll[0] + "-" + player.roll[1] + "-" + player.roll[2] + 
-								  "\nThe banker rolled: " + banker.roll[0] + "-" + banker.roll[1] + "-" + banker.roll[2];
-			compare(rank(player, player.roll.sort()), rank(banker, banker.roll.sort());
+			result = [randNum1, randNum2, randNum3];
 		}
 
 	}, 150);
+
+	return result;
 }
 
-function rank(playerOrBanker, roll){
+function rank(roll){
+	var rank = 0;
 	if(roll[0] === 4 && roll[1] === 5 && roll[2] === 6){
-		playerOrBanker.rank = 14;
+		rank = 14;
+	} else if (roll[0] === roll[2]){
+		rank = roll[0] + 7;
+	} else if (roll[0] === roll[1] && roll[1] !== roll[2]){
+		rank = roll[2] + 1;
+	} else if (roll[0] !== roll[1] && roll[1] === roll[2]){
+		rank = roll[0] + 1;
+	} else if (roll[0] === 1 && roll[1] === 2 && roll[2] === 3){
+		rank = 1;
+	} else {
+		rank = 0;
 	}
-	return playerOrBanker.rank;
+	return rank;
 }
 
 function compare(playerRank, bankerRank){
+	// 1 = win, 2 = tie, 3 = lose
 
+	var roundResult = 0;
+
+	if(player.rank === 14){
+		roundResult = 1;
+	} else if (player.rank === banker.rank){
+		roundResult = 2;
+	} else if (player.rank > banker.rank){
+		roundResult = 1;
+	} else {
+		roundResult = 3;
+	}
+	return roundResult;
 }
 
-function payout(){
+function payout(roundResult){
+	if(roundResult === 1){
+		player.chips += pot;
+	} else if (roundResult === 2){
+		player.chips += pot / 2;
+		banker.chips += pot / 2;
+	} else {
+		banker.chips += pot;
+	}
+	update();
+}
 
+function reset(){
+	el_bet.disabled = false;
+	el_bet.style.backgroundColor = "rgba(100, 255, 100, 0.5)";
+
+	el_roll.disabled = true;
 }
 
 // Event Listeners
@@ -169,4 +215,13 @@ el_roll.addEventListener("click", roll, false);
 
 // Initialize
 
-alert(rank(player, [4, 5, 6]));
+// while(gameEnd === false){
+
+	// player.roll = roll();
+	// banker.roll = roll();
+	// el_status.innerHTML = "The player rolled: " + player.roll[0] + "-" + player.roll[1] + "-" + player.roll[2] + 
+	// 					  "\nThe banker rolled: " + banker.roll[0] + "-" + banker.roll[1] + "-" + banker.roll[2];
+	// player.rank = rank(player.roll.sort());
+	// banker.rank = rank(banker.roll.sort());
+
+// }
